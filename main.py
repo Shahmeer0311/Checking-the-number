2c97b14f1987416e5e4a7fb9580f9c8a5190d438
Shahmeer0311 <22alis@students.watfordboys.org>
2024-12-01 14:29:49 +0000

    checking

---
+++ main.py
@@ -0,0 +1,7 @@
+Number = int(input("What is the number"))
+if (Number > 15):
+    print(Number,"is more than 15")
+elif (Number == 15):
+    print(Number,"is 15")
+else:
+     print(Number,"is less than 15")
